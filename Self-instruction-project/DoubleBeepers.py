8500f2a353b7216ce14ce54004c1db13eb2dd32b
Alice-HWANG <131263114+Alice-HWANG@users.noreply.github.com>
2023-05-06 22:48:11 +0800

    Add files via upload

---
+++ Self-instruction-project/DoubleBeepers.py
@@ -0,0 +1,59 @@
+"""
+File: DoubleBeepers.py
+Name:ALICE HWANG
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will double the beepers
+    """
+    move()
+    double_beepers()
+    put_beepers_back()
+    Karel_goes_home()
+
+def put_beepers_back():
+    turn_around()
+    move()
+    while on_beeper():
+        pick_beeper()
+        turn_around()
+        move()
+        put_beeper()
+        turn_around()
+        move()
+
+def double_beepers():
+    while on_beeper():
+        # old beepers,facing East.
+        pick_beeper()
+        move()
+        put_beeper()
+        put_beeper()
+        # new beepers,facing East.
+        turn_around()
+        move()
+        # old beepers,facing West.
+        turn_around()
+
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+
+def Karel_goes_home():
+    turn_around()
+    move()
+
+
+
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
